b662fd98d2d9d3e469b668169ded4ec73bcf004f
daniel-palmen <daniel.palmen@gmail.com>
2025-08-26 10:48:44 +0300

    update esim3.2.py

--- Ohjelmisto-1/mod-03/esim3.2.py
+++ Ohjelmisto-1/mod-03/esim3.2.py
@@ -1,7 +1,9 @@
 Summa = 0
 Kulutus = float(input('Anna sähkönkulutus kilowattitunteina: '))
 
-if Kulutus <= 50:
+if Kulutus < 0:
+    Summa = 0
+elif Kulutus <= 50:
     Summa = Kulutus * 0.1
 elif Kulutus <= 200:
     Kulutus = Kulutus - 50
