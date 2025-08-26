40bb6bda5fe022646682f74a22504201ca84b743
daniel-palmen <daniel.palmen@gmail.com>
2025-08-26 18:16:24 +0300

    update teht9.4.py

--- Ohjelmisto-1/mod-09/teht9.4.py
+++ Ohjelmisto-1/mod-09/teht9.4.py
@@ -17,18 +17,22 @@ class auto:
         kuljettuMatka = self.tamanhetkinenNopeus * aika
         self.kuljettuMatka = self.kuljettuMatka + kuljettuMatka
 
-AutojenMaara = 11
+AutojenMaara = 10
+Maali = 10000
 Lopetus = 0
-Directory = {}
-for i in range(1, AutojenMaara):
+Lista = []
+for i in range(0, AutojenMaara):
     Huippu = random.randint(100,200)
-    rekisteri = 'ABC-{0}'.format(i)
-    Directory['Auto{0}'.format(i)] = auto(rekisteri, Huippu, 0 , 0)
-print(Directory['Auto1'])
-print(Directory['Auto2'])
+    rekisteri = 'ABC-{0}'.format(i + 1)
+    Lista.append(auto(rekisteri, Huippu, 0, 0))
+print(Lista[0].huippunopeus)
 while Lopetus == 0:
-    for i in range(1, AutojenMaara):
+    for i in range(0, AutojenMaara):
         muutos = random.randint(-10, 15)
-        Auto1.kiihdyta(muutos)
-        print(Directory['Auto1'])
-        Lopetus = 1
+        Lista[i].kiihdyta(muutos)
+        Lista[i].kulje(1)
+        if Lista[i].kuljettuMatka >= Maali:
+            print(f'Auto {Lista[i].rekisteri} voitti kisan.')
+            Lopetus = 1
+for i in range(0, AutojenMaara):
+    print(Lista[i])
