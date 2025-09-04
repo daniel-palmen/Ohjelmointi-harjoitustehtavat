c176e9632eee730711f7c304595881c8a9f8a103
daniel-palmen <daniel.palmen@gmail.com>
2025-09-04 11:42:47 +0300

    update teht10.2.py

--- Ohjelmisto-1/mod-10/teht10.2.py
+++ Ohjelmisto-1/mod-10/teht10.2.py
@@ -1,66 +1,66 @@
 
 class hissi:
-    def __init__(self, AlinKerros, YlinKerros, NykyinenKerros):
-        self.AlinKerros = AlinKerros
-        self.YlinKerros = YlinKerros
-        self.NykyinenKerros = NykyinenKerros
+    def __init__(self, alinKerros, ylinKerros, nykyinenKerros):
+        self.alinKerros = alinKerros
+        self.ylinKerros = ylinKerros
+        self.nykyinenKerros = nykyinenKerros
     def __str__(self):
-        return (f'Alin Kerros:{self.AlinKerros} YlinKerros:{self.YlinKerros} Nykyinen Kerros:{self.NykyinenKerros}')
-    def siirry_kerrokseen(self, Muutos):
-        if Muutos > self.NykyinenKerros:
-            for i in range (self.NykyinenKerros, Muutos):
-                if self.NykyinenKerros == self.YlinKerros:
-                    print(f'Ylin kerros {self.YlinKerros}')
+        return (f'Alin Kerros:{self.alinKerros} YlinKerros:{self.ylinKerros} Nykyinen Kerros:{self.nykyinenKerros}')
+    def siirry_kerrokseen(self, muutos):
+        if muutos > self.nykyinenKerros:
+            for i in range (self.nykyinenKerros, muutos):
+                if self.nykyinenKerros == self.ylinKerros:
+                    print(f'Ylin kerros {self.ylinKerros}')
                     break
                 self.kerros_ylös()
-        if Muutos < self.NykyinenKerros:
-            Alaspain = self.NykyinenKerros - Muutos
-            for i in range (0, Alaspain):
-                if self.NykyinenKerros == self.AlinKerros:
-                    print(f'Alin Kerros {self.AlinKerros}')
+        if muutos < self.nykyinenKerros:
+            alaspain = self.nykyinenKerros - muutos
+            for i in range (0, alaspain):
+                if self.nykyinenKerros == self.alinKerros:
+                    print(f'Alin Kerros {self.alinKerros}')
                     break
                 self.kerros_alas()
     def kerros_ylös(self):
-       self.NykyinenKerros = self.NykyinenKerros + 1
-       print(f'Nykyinen Kerros {self.NykyinenKerros}')
+       self.nykyinenKerros = self.nykyinenKerros + 1
+       print(f'Nykyinen Kerros {self.nykyinenKerros}')
     def kerros_alas(self):
-        self.NykyinenKerros = self.NykyinenKerros - 1
-        print(f'Nykyinen Kerros {self.NykyinenKerros}')
+        self.nykyinenKerros = self.nykyinenKerros - 1
+        print(f'Nykyinen Kerros {self.nykyinenKerros}')
 class talo:
-    def __init__(self, AlinKerros, YlinKerros, Hissit):
-        self.AlinKerros = AlinKerros
-        self.YlinKerros = YlinKerros
-        self.Hissit = []
+    def __init__(self, alinKerros, ylinKerros, hissit):
+        self.alinKerros = alinKerros
+        self.ylinKerros = ylinKerros
+        self.hissit = []
     def __str__(self):
-        return (f'Alin kerros:{self.AlinKerros} Ylin kerros:{self.YlinKerros} Hissit:{self.Hissit}')
-    def aja_hissiä (self, HissiNro, Muutos):
-        self.Hissit[HissiNro].siirry_kerrokseen(Muutos)
+        return (f'Alin kerros:{self.alinKerros} Ylin kerros:{self.ylinKerros} Hissit:{self.hissit}')
+    def aja_hissiä (self, hissiNro, muutos):
+        self.hissit[hissiNro].siirry_kerrokseen(muutos)
     def palohälytys (self):
-        for i in self.Hissit:
-            while self.Hissit[i].NykyinenKerros != self.AlinKerros: #EI TOIMI
-                Muutos = -10
-                Muutos = Muutos - 10
-                self.aja_hissiä(i, Muutos)
+        for i in self.hissit:
+            while self.hissit[i].NykyinenKerros != self.alinKerros: #EI TOIMI
+                muutos = -10
+                muutos = muutos - 10
+                self.aja_hissiä(i, muutos)
 
-AlinKerros = 0
-YlinKerros = 10
-HissienMaara = 3
+alinKerros = 0
+ylinKerros = 10
+hissienMaara = 3
 
-Talo1 = talo(AlinKerros, YlinKerros,0)
-for i in range(0, HissienMaara):
-    i = hissi(AlinKerros, YlinKerros, 0)
-    Talo1.Hissit.append(i)
+talo1 = talo(alinKerros, ylinKerros,0)
+for i in range(0, hissienMaara):
+    i = hissi(alinKerros, ylinKerros, 0)
+    talo1.hissit.append(i)
 
-HissiNro = 0
-Muutos = 3
+hissiNro = 0
+muutos = 3
 
-Talo1.aja_hissiä(HissiNro, Muutos)
+talo1.aja_hissiä(hissiNro, muutos)
 
 #Testi
-for i in range(0,HissienMaara):
-    print(Talo1.Hissit[i])
+for i in range(0,hissienMaara):
+    print(talo1.hissit[i])
 
-Talo1.palohälytys()
+talo1.palohälytys()
 
-for i in range(0, HissienMaara):
-    print(Talo1.Hissit[i])
+for i in range(0, hissienMaara):
+    print(talo1.hissit[i])
