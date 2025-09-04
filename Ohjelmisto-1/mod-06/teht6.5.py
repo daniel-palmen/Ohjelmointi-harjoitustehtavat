2b2316225158d6ed3acc0d744d9fe95c9040d545
daniel-palmen <daniel.palmen@gmail.com>
2025-09-04 10:54:02 +0300

    update teht6.5.py

--- Ohjelmisto-1/mod-06/teht6.5.py
+++ Ohjelmisto-1/mod-06/teht6.5.py
@@ -10,7 +10,7 @@ print('Anna 5 lukua: ')
 while len(Lista) < 5:
     Luku = float(input())
     Lista.append(Luku)
-if len(Lista) == 5:
-    Parilliset = muunnin(Lista)
-    print(f'{Lista} Annetut luvut.')
-    print(f'{Parilliset} Parilliset luvut.')
+
+Parilliset = muunnin(Lista)
+print(f'{Lista} Annetut luvut.')
+print(f'{Parilliset} Parilliset luvut.')
