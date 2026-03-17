aa94bba829502f74c7d16a98faa9116f642a3aa0
Daniel <daniel.palmen@gmail.com>
2026-03-17 14:54:42 +0200

    update PE_Task2.1.py

--- Laitteisto-2/Projekti_TT/week2/PE_Task2.1.py
+++ Laitteisto-2/Projekti_TT/week2/PE_Task2.1.py
@@ -8,7 +8,7 @@ true = True
 dataA = 0
 dataB = 0
 slope2 = 0
-time = 0
+v_time = 0
 cum_time = 0
 first = 0
 three = 0
@@ -22,30 +22,32 @@ oled.text('Task 2.1', 0, 0, 1)
 oled.text('press SW1', 0, 8, 1)
 oled.show()
 
-while true:
+while True:
     if button() == 0:
+        time.sleep(0.5)
         for _ in range(1000):
             if fifo.has_data():
                 dataB = dataA
                 dataA = fifo.get()
-                time = time + 1
+                v_time = v_time + 1
                 if dataA > 10000:
                     slope1 = dataA - dataB
-                    if slope2 >= 0 and slope1 <= 0 and time >= 10:
+                    if slope2 >= 0 and slope1 <= 0 and v_time >= 10:
                         if first == 0:
                             first = 1
                             print(dataA)
                         else:
-                            print(f'samples: {time}')
-                            time = time/250
-                            cum_time = cum_time + time
-                            print(f'time: {time}s')
+                            print(f'samples: {v_time}')
+                            v_time = v_time/250
+                            cum_time = cum_time + v_time
+                            print(f'time: {v_time}s')
                             print(dataA)
                             three = three + 1
-                        time = 0
+                        v_time = 0
                     slope2 = slope1
             if three >= 3:
                 frequency = 1/(cum_time/3)
                 print(f'\nFrequency: {frequency}Hz')
-                true = False
+                cum_time = 0
+                three = 0
                 break
