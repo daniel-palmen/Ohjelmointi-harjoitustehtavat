5a3301967e6db047ecafd22f6092900921a5541b
Daniel <daniel.palmen@gmail.com>
2026-03-17 08:55:40 +0200

    update PE_Task1.1.py

--- Laitteisto-2/Projekti_TT/PE_Task1.1.py
+++ Laitteisto-2/Projekti_TT/PE_Task1.1.py
@@ -2,17 +2,31 @@ import time
 from machine import UART, Pin, I2C, Timer, ADC
 from ssd1306 import SSD1306_I2C
 
-button = Pin(9, Pin.IN, Pin.PULL_UP)
+button_R = Pin(9, Pin.IN, Pin.PULL_UP)
+button_L = Pin(7, Pin.IN, Pin.PULL_UP)
 i2c = I2C(1, scl=Pin(15), sda=Pin(14), freq=400000)
 oled_width = 128
 oled_height = 64
 oled = SSD1306_I2C(oled_width, oled_height, i2c)
+position = 52
+delay = 0.05
 oled.fill(0)
-oled.text('Hello World', 0, 0, 1)
+oled.text('<=>', position, 54, 1)
 oled.show()
-oled.fill(0)
-oled.text('Button pressed', 0, 10, 1)
 
-while button() == 1:
-    time.sleep(0.05)
-oled.show()
+while True:
+    if button_R() == 0:
+        time.sleep(delay)
+        if position < 105:
+            position = position + 1
+        oled.fill(0)
+        oled.text('<=>', position, 54, 1)
+        oled.show()
+        
+    if button_L() == 0:
+        time.sleep(delay)
+        if position >= 0:
+            position = position - 1
+        oled.fill(0)
+        oled.text('<=>', position, 54, 1)
+        oled.show()
