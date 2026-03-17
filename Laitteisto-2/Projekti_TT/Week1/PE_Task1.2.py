ae7b8dd12b7f0bf1599b48f06922e22fb02d66a2
Daniel <daniel.palmen@gmail.com>
2026-03-17 09:47:33 +0200

    update PE_Task1.2.py

--- Laitteisto-2/Projekti_TT/Week1/PE_Task1.2.py
+++ Laitteisto-2/Projekti_TT/Week1/PE_Task1.2.py
@@ -1,13 +1,21 @@
 import time
-import machine
-import ssd1306
+from machine import UART, Pin, I2C, Timer, ADC
+from ssd1306 import SSD1306_I2C
 
 i2c = I2C(1, scl=Pin(15), sda=Pin(14), freq=400000)
 oled_width = 128
 oled_height = 64
 oled = SSD1306_I2C(oled_width, oled_height, i2c)
+row = 0
 
 oled.fill(0)
-oled.text('test',0,0,1)
-oled.scroll(1)
-oled.text('test 2',0,0,1)
+
+while True:
+    user_input = input()
+    oled.text(user_input,0,row,1)
+    oled.show()
+    row = row + 8
+    if row >= 64:
+        oled.scroll(0,-8)
+        row = 56
+        oled.fill_rect(0, 56, 128, 8, 0)
