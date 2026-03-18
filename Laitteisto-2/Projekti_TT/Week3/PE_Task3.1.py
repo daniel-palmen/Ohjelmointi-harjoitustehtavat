76cdb806f377ce2bdccb1275a5dc3b6790e99c1d
Daniel <daniel.palmen@gmail.com>
2026-03-18 14:44:49 +0200

    update PE_Task3.1.py and PE_Task3.2.py

--- Laitteisto-2/Projekti_TT/Week3/PE_Task3.1.py
+++ Laitteisto-2/Projekti_TT/Week3/PE_Task3.1.py
@@ -1,21 +1,39 @@
 import time
 from fifo import Fifo
-from machine import UART, Pin, I2C, Timer, ADC
+from machine import UART, Pin, I2C, Timer, ADC, PWM
 from ssd1306 import SSD1306_I2C
 
 def main():           
     rot = Encoder(10, 11, 12)
+    led = Led(22)
+    led.light.freq(1000)
+    fade_step = 1
     switch = 0
     while True:
+        led.light.duty_u16(int(led.brightness * 65535 / 255))
         if rot.press() == 0:
             time.sleep(0.5)
             if switch == 0:
                 switch = 1
+                led.brightness = led.memory
             else:
                 switch = 0
+                led.memory = led.brightness
+                led.brightness = 0
         if rot.fifo.has_data():
             if switch == 1:
-                print(rot.fifo.get())
+                if led.brightness < 0:
+                    led.brightness = 0
+                elif led.brightness > 50:
+                    led.brightness = 50
+                elif 0 <= led.brightness <= 50:
+                    change = rot.fifo.get()
+                    if led.brightness == 0 and change == -1:
+                        led.brightness = 0
+                    else:
+                        led.brightness = led.brightness + (fade_step * change)
+                print(led.brightness)
+                
             else:
                 rot.fifo.get()
 
@@ -32,5 +50,11 @@ class Encoder:
             self.fifo.put(-1)
         else:
             self.fifo.put(1)
+
+class Led:
+    def __init__(self, pin):
+        self.light = PWM(pin)
+        self.brightness = 0
+        self.memory = 0
 
 main()
---
+++ Laitteisto-2/Projekti_TT/Week3/PE_Task3.2.py
@@ -0,0 +1,95 @@
+import time
+from fifo import Fifo
+from machine import UART, Pin, I2C, Timer, ADC, PWM
+from ssd1306 import SSD1306_I2C
+
+def main():           
+    rot = Encoder(10, 11, 12)
+    led1 = Led(22)
+    led2 = Led(21)
+    led3 = Led(20)
+    led1.light.freq(1000)
+    led2.light.freq(1000)
+    led3.light.freq(1000)
+    oled_width = 128
+    oled_height = 64
+    i2c = I2C(1, scl=Pin(15), sda=Pin(14), freq=400000)
+    oled = SSD1306_I2C(oled_width, oled_height, i2c)
+    chooser = 0
+
+    oled.fill(0)
+    oled.text(f'LED1 - {led1.state}', 8, 0, 1)
+    oled.text(f'LED2 - {led2.state}', 8, 8, 1)
+    oled.text(f'LED3 - {led3.state}', 8, 16, 1)
+    oled.text('[', 0, chooser * 8, 1)
+    oled.text(']', 88, chooser * 8, 1)
+    oled.show()
+
+    
+    while True:
+        led1.light.duty_u16(int(led1.brightness * 65535 / 255))
+        led2.light.duty_u16(int(led2.brightness * 65535 / 255))
+        led3.light.duty_u16(int(led3.brightness * 65535 / 255))
+
+        if rot.press() == 0:
+            time.sleep(0.2)
+            if chooser == 0:
+                if led1.brightness == 10:
+                    led1.off()
+                else:
+                    led1.on()
+                    oled.fill.rect
+                    oled.text(f'LED1 - {led1.state}', 8, 0, 1)
+            if chooser == 1:
+                if led2.brightness == 10:
+                    led2.off()
+                else:
+                    led2.on()
+            if chooser == 2:
+                if led3.brightness == 10:
+                    led3.off()
+                else:
+                    led3.on()
+
+        if rot.fifo.has_data():
+            oled.text('[', 0, chooser * 8, 0)
+            oled.text(']', 88, chooser * 8, 0)
+            info = rot.fifo.get()
+            if info == 1:
+                if chooser < 2:
+                    chooser = chooser + 1
+                else:
+                    chooser = 2
+            elif info == -1:
+                if chooser > 0:
+                    chooser = chooser - 1
+                else:
+                    chooser = 0
+            oled.text('[', 0, chooser * 8, 1)
+            oled.text(']', 88, chooser * 8, 1)
+            oled.show()
+class Encoder:
+    def __init__(self, rot_a, rot_b, rot_press):
+        self.a = Pin(rot_a, mode = Pin.IN)
+        self.b = Pin(rot_b, mode = Pin.IN)
+        self.press = Pin(rot_press, Pin.IN, Pin.PULL_UP)
+        self.fifo = Fifo(30, typecode = 'i')
+        self.a.irq(handler = self.handler, trigger = Pin.IRQ_RISING, hard = True)
+    def handler(self, pin):
+        if self.b():
+            self.fifo.put(-1)
+        else:
+            self.fifo.put(1)
+
+class Led:
+    def __init__(self, pin):
+        self.light = PWM(pin)
+        self.brightness = 0
+        self.state = 'OFF'
+    def on(self):
+        self.brightness = 10
+        self.state = 'ON'
+    def off(self):
+        self.brightness = 0
+        self.state = 'OFF'
+main()
