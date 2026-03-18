4b7e853d4e26d2cd9dabf49becbde5f1450f5b9a
Daniel <daniel.palmen@gmail.com>
2026-03-18 12:01:20 +0200

    update PE_Task2.3.py

--- Laitteisto-2/Projekti_TT/week2/PE_Task2.3.py
+++ Laitteisto-2/Projekti_TT/week2/PE_Task2.3.py
@@ -5,10 +5,26 @@ from ssd1306 import SSD1306_I2C
 
 def main():
     fifo = Filefifo(10, name = 'sinewave_250Hz_03.txt') #250Hz
-    threshold = thresholder(fifo)
-    print(threshold)
-    frequency = freq_finder(fifo, threshold)
-    print(frequency)
+    i2c = I2C(1, scl=Pin(15), sda=Pin(14), freq=400000)
+    button = Pin(8, Pin.IN, Pin.PULL_UP)
+    oled_width = 128
+    oled_height = 64
+    oled = SSD1306_I2C(oled_width, oled_height, i2c)
+    oled.fill(0)
+    oled.text('Task 2.2', 0, 0, 1)
+    oled.text('press SW1', 0, 8, 1)
+    oled.show()
+    while True:
+        if button() == 0:
+            time.sleep(0.1)
+            oled.fill(0)
+            threshold = thresholder(fifo)
+            print(f'threshold: {threshold}')
+            oled.text(f'threshold:{threshold}', 0, 0, 1)
+            frequency = round(freq_finder(fifo, threshold), 3)
+            print(f'frequency:{frequency}s')
+            oled.text(f'frequency:{frequency}s', 0, 8, 1)
+            oled.show()
     return
 
 def thresholder(fifo): #reads first 2 seconds and sets threshold
@@ -16,7 +32,7 @@ def thresholder(fifo): #reads first 2 seconds and sets threshold
     data_max = 0
     data_min = 0
     data_setter = 0
-    for _ in range(250*2):
+    for i in range(250*2):
                 if fifo.has_data():
                     data_new = fifo.get()
                     if data_setter == 0:
@@ -28,7 +44,6 @@ def thresholder(fifo): #reads first 2 seconds and sets threshold
                     elif data_new < data_min:
                         data_min = data_new
     threshold = int((data_max + data_min)/2)
-    print(data_max,data_min)
     return threshold
 
 def freq_finder(fifo, threshold):
@@ -36,19 +51,21 @@ def freq_finder(fifo, threshold):
     first = 0
     frequency = 0
     times = 0
-    for _ in range(250*2):
+    going_up = 0
+    for i in range(250*10):
             if fifo.has_data():
                 data_new = fifo.get()
-                if first = 1:
+                if first == 1:
                     counter = counter + 1
                 if data_new < threshold:
-                    going up = 1
-                elif going up == 1 and data_new > theshold:
-                    if first = 1:
-                    times = times + 1
+                    going_up = 1
+                elif going_up == 1 and data_new > threshold:
+                    going_up = 0
+                    if first == 1:
+                        times = times + 1
                     else:
                         first = 1
-    
+    frequency = (counter / times) / 250
     return frequency
 
 main()
