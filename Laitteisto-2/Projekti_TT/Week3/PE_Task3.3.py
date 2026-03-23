0487617fa325b7ddde733459f73ee366ca38293a
Daniel <daniel.palmen@gmail.com>
2026-03-23 15:04:48 +0200

    update PE_Task3.3.py

--- Laitteisto-2/Projekti_TT/Week3/PE_Task3.3.py
+++ Laitteisto-2/Projekti_TT/Week3/PE_Task3.3.py
@@ -6,7 +6,6 @@ from ssd1306 import SSD1306_I2C
 
 def main():
     rot = Encoder(10, 11, 12)
-    text_file = Filefifo(10, name = 'week3_data.txt')
     oled_width = 128
     oled_height = 64
     i2c = I2C(1, scl=Pin(15), sda=Pin(14), freq=400000)
@@ -14,10 +13,11 @@ def main():
     
     low = 0
     high = 128
+    new_value = 0
     
     oled.fill(0)
-    data_max, data_min = min_maxer(text_file)
-    scaler(text_file, data_max, data_min, oled, low, high)
+    data_max, data_min = min_maxer()
+    scaler(data_max, data_min, oled, low, high)
     oled.show()
     
     while True:
@@ -25,14 +25,27 @@ def main():
             value = rot.fifo.get()
             print(value)
             if value == 1:
-                low += low
-                high += high
+                low = low + 1
+                high = high + 1
+                if high <= 1000:
+                    new_value = high
+                    adder(new_value, oled, data_min, data_max)
+                    oled.scroll(-1, 0)
+                elif high > 1000:
+                    low = 871
+                    high = 1000
             else:
-                low -= low
-                high -= high
-            oled.fill(0)
-            scaler(text_file, data_max, data_min, oled, low, high)
+                low = low - 1
+                high = high - 1
+                if low > 0:
+                    new_value = low
+                    adder_b(new_value, oled, data_min, data_max)
+                    oled.scroll(1, 0)
+                elif low <= 0:
+                    low = 0
+                    high = 128
             oled.show()
+
 class Encoder:
     def __init__(self, rot_a, rot_b, rot_press):
         self.a = Pin(rot_a, mode = Pin.IN)
@@ -47,26 +60,28 @@ class Encoder:
         else:
             self.fifo.put(1)
             
-def min_maxer(text_file): #reads first 10s seconds and sets max and low
+def min_maxer(): #reads first 10s seconds and sets max and low
     data_new = 0
     data_max = 0
     data_min = 0
     data_setter = 0
+    text_file = Filefifo(10, name = 'week3_data.txt')
     for _ in range(1000):
                 if text_file.has_data():
                     data_new = text_file.get()
                     if data_setter == 0:
-                        data_max = data_new
+                        data_max = data_new + 1
                         data_min = data_new
                         data_setter = 1
                     elif data_new > data_max:
                         data_max = data_new
                     elif data_new < data_min:
-                        data_min = data_new
+                        data_min = data_new                   
     return data_max, data_min
 
-def scaler(text_file, data_max, data_min, oled, low, high): #scales 10s of data
+def scaler(data_max, data_min, oled, low, high): #scales 10s of data
     placer = 0
+    text_file = Filefifo(10, name = 'week3_data.txt')
     for i in range(low, high):
         if text_file.has_data():
             data_new = text_file.get()
@@ -80,5 +95,33 @@ def screener(data_scaled, oled, placer): #screens data to oled
     placer = placer + 1
     return placer
 
+def adder(new_value, oled, data_min, data_max):
+    placer = 126
+    text_file = Filefifo(10, name = 'week3_data.txt')
+    for i in range(0, new_value):
+        if text_file.has_data():
+            data_new = text_file.get()
+    data_scaled = ((data_new - data_min) / (data_max - data_min)) * (62)
+    data_scaled = round(data_scaled)
+    print(new_value)
+    print(data_new)
+    oled.fill_rect(126, 0, 2, 64, 0)
+    placer = screener(data_scaled, oled, placer) #screens data to oled
+    return
+
+def adder_b(new_value, oled, data_min, data_max):
+    placer = 0
+    text_file = Filefifo(10, name = 'week3_data.txt')
+    for i in range(0, new_value):
+        if text_file.has_data():
+            data_new = text_file.get()
+    data_scaled = ((data_new - data_min) / (data_max - data_min)) * (62)
+    data_scaled = round(data_scaled)
+    print(new_value)
+    print(data_new)
+    oled.fill_rect(0, 0, 2, 64, 0)
+    placer = screener(data_scaled, oled, placer) #screens data to oled
+    
+    return
 
 main()
