437ea9349a838e2609296038ea1b0a9f04eddd27
Daniel <daniel.palmen@gmail.com>
2026-04-21 12:57:25 +0300

    update PE_Task4.3.py

--- Laitteisto-2/Projekti_TT/week4/PE_Task4.3.py
+++ Laitteisto-2/Projekti_TT/week4/PE_Task4.3.py
@@ -1,26 +1,30 @@
 from fifo import Fifo
-from machine import ADC, Pin
+from machine import ADC, Pin, I2C
 from piotimer import Piotimer
 from led import Led
 import micropython
+from ssd1306 import SSD1306_I2C
 
 class isr_adc:
     def __init__(self, adc_pin_nr, led_pin_nr):
         self.av = ADC(adc_pin_nr)
         self.led = Led(led_pin_nr)
-        self.fifo = Fifo(500)
+        self.fifo = Fifo(750)
         self.factor = 0.85
         self.led.off()
         self.ppi = 0 #yksi väli
         self.MS_IN_MINUTE = 60000
+        
     def handler(self, tid):
         self.fifo.put(self.av.read_u16())
+        
     def calculate_threshold(self):
         min_val = min(self.fifo.data)
         max_val = max(self.fifo.data)
         h = max_val-min_val
         threshold = min_val + self.factor * h
         return int(threshold)
+    
     def calculate_hr(self):
         ppi_ms = self.ppi * 4
         hr = int(self.MS_IN_MINUTE / ppi_ms)
@@ -52,27 +56,42 @@ prev_value = 0
 peak_found = False
 rot = Encoder(10, 11, 12)
 
-ia = isr_adc(adc_pin_nr, led_pin_nr)
-tmr = Piotimer(mode = Piotimer.PERIODIC, freq = sample_rate, callback = ia.handler)
+start_button = Pin(8, Pin.IN, Pin.PULL_UP)
+started = 0
 
-#init threshold
-#while fifo_counter < sample_rate * 2:
-#    if not ia.fifo.empty():
-#        value = ia.fifo.get()
-#        fifo_counter += 1
+i2c = I2C(1, scl=Pin(15), sda=Pin(14), freq=400000)
+oled_width = 128
+oled_height = 64
+oled = SSD1306_I2C(oled_width, oled_height, i2c)
 
-th = ia.calculate_threshold()
-#fifo_counter = 0
+oled.fill(0)
+oled.text('Task 4.3', 0, 0, 1)
+oled.text('press SW1', 0, 8, 1)
+oled.show()
 
+while started == 0:
+    if start_button() == 0:
+        print('test1')
+        ia = isr_adc(adc_pin_nr, led_pin_nr)
+        tmr = Piotimer(mode = Piotimer.PERIODIC, freq = sample_rate, callback = ia.handler)
+        #init threshold
+        while fifo_counter + 1 < sample_rate * 2:
+            if not ia.fifo.empty():
+                value = ia.fifo.get()
+                fifo_counter += 1
+        th = ia.calculate_threshold()
+        fifo_counter = 0
+        started = 1
+        
 while True:
     if not ia.fifo.empty():
         value = ia.fifo.get()
         #print(value, th)
         fifo_counter += 1
         ia.ppi += 1
-        if fifo_counter >= sample_rate:
-            th = ia.calculate_threshold()
-            fifo_counter = 0
+#        if fifo_counter >= sample_rate * 2:
+#            th = ia.calculate_threshold()
+#            fifo_counter = 0
         
         if value > th: #yli rajan
             if passed_th == False: #ensimmäinen ylitys
@@ -84,17 +103,18 @@ while True:
                     if hr < 240 and hr > 30:
                         print(hr)
                     peak_found = True
-                    ia.ppi = 0
-                
+                    ia.ppi = 0      
         else: #alle rajan
             if passed_th == True:
                 ia.led.off()
                 passed_th = False
                 peak_found = False
-
         prev_value = value
-        if not rot.fifo.empty():
-            ia.factor = ia.factor - rot.fifo.get() * 0.01
-            print(f'factor changed to {ia.factor}')
+        
+    if not rot.fifo.empty():
+        #ia.factor = ia.factor - rot.fifo.get() * 0.01
+        th = th - rot.fifo.get()*500
+        print(f'Threshold chaged to {th}')
+        #print(f'factor changed to {ia.factor:.2f}')
 
 #lisää liukuva keskiarvo ja rajat hr muutokselle ja ehkä hr muutoksen raja
